be855f194e996dc44cd7950eae4b8da55ba66566
Mandarin <zurual@mail.ru>
2023-08-16 22:59:49 +0300

    end game

--- main.py
+++ main.py
@@ -1,7 +1,92 @@
 #Hangman
 import random
 
-
+#const
+HANGMAN = (
+"""
+------
+|    |
+|
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+------
+|    |
+|    O  |
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+------
+|    |
+|    O
+|   -+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+------
+|    |
+|    O
+|  /-+-/
+|
+|
+|
+|
+|
+--------
+""",
+"""
+------
+|    |
+|    O
+|  /-+-/
+|    |
+|
+|
+|
+|
+--------
+""",
+"""
+------
+|    |
+|    O
+|  /-+-/
+|    |
+|    |
+|   |
+|   |
+|
+--------
+""",
+"""
+------
+|    |
+|    O
+|  /-+-/
+|    |
+|    |
+|   | |
+|   | |
+|
+--------
+"""
 )
 MAX_WRONG = len(HANGMAN) - 1
 WORDS = ("СТОЛОВАЯ", "РЕСТОРАН", "ШКОЛА", "СПРАВКА", "АНЕКДОТ", "ЗАВТРАК", "БИЗНЕС")
@@ -24,3 +109,36 @@ print(
 рисованного человечка!
 """
 )
+while wrong < MAX_WRONG and so_far != word:
+    print(HANGMAN[wrong])
+    print("\nВы уже предлагали следующие буквы:\n", used)
+    print("\nОтгаданное вами в слове сейчас выглядит так:\n", so_far)
+    #пользовательский ввод
+    guess = input("\n\nВведите букву: ")
+    guess = guess.upper()
+    while guess in used:
+        print("Вы уже предлагали букву: ", guess)
+        guess = input("\n\nВведите букву: ")
+        guess = guess.upper()
+    used.append(guess)
+    #проверка наличия буквы в слове
+    if guess in word:
+        print("\nДа! Буква", guess, "есть в слове!")
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+        so_far = new
+    else:
+        print("\nК сожалению, буквы", guess, "нет в слове.")
+        wrong += 1
+#завершение игры
+if wrong == MAX_WRONG:
+    print(HANGMAN[wrong])
+    print("\nЧеловеска повесили!")
+else:
+    print("\nВы отгадали!")
+print("\nБыло загаданно слово", word, ".")
+input("\n\nPress ENTER to EXIT.")
